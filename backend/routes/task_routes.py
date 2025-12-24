33c60abf63ab3e7716d074131f4c22994abe005a
clivedev88m <clive.dev88@gmail.com>
2025-12-24 10:15:45 -0300

    Atualização da estrutura para buscar tasks por ID

--- backend/routes/task_routes.py
+++ backend/routes/task_routes.py
@@ -12,3 +12,12 @@ def get_tasks_by_project(project_id):
     tasks = taskService.list_tasks_by_project(project_id)
     return jsonify(tasks)
 
+@task_bp.route('/projects/<int:project_id>/tasks/<int:task_id>', methods=['GET'])
+def get_task_by_id(project_id, task_id):
+    # project = projectService.get_by_id(project_id)
+    task = taskService.get_task(project_id, task_id)
+
+    if task is None:
+        return jsonify({'Error':'Task não existe para esse projeto'}), 404
+    
+    return jsonify(task)
