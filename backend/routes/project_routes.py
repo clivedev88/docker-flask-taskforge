8a1d250c5883dc77c6c8c693c9d027bc4fc32ded
clivedev88m <clive.dev88@gmail.com>
2025-12-24 08:31:19 -0300

    desafio pessoal: criação de estrutura para buscar projetos por id

--- backend/routes/project_routes.py
+++ backend/routes/project_routes.py
@@ -1,10 +1,17 @@
 from flask import Blueprint, jsonify
 from services.project_service import ProjectService
 
-project_bp = Blueprint('projects', __name__)
+project_bp = Blueprint('project', __name__)
 service = ProjectService()
 
-@project_bp.route('/projects', methods=['GET'])
-def get_projects():
-    projects = service.list_projects()
+@project_bp.route('/projects',  methods=['GET'])
+def get_all_projects():
+    projects = service.get_projects()
     return jsonify(projects)
+
+@project_bp.route('/projects/<int:project_id>', methods=['GET'])
+def get_project_by_id(project_id):
+    project = service.get_by_id(project_id)
+    # if project is None:
+    #     return jsonify({'Error':'Projeto não existe'}), 404
+    return jsonify(project)
